406e83debda826dd146bc27173f5e35c905fb3ba
StasKuleshov <54893283+StasKuleshov@users.noreply.github.com>
2019-09-10 18:32:25 +0300

    Add files via upload

--- lab/task_22.py
+++ lab/task_22.py
@@ -9,14 +9,16 @@ def task_5_10():
     c1 = 1
     while not wall_is_on_the_right():
          c1 += 1
-         move_right() 
-    move_left(c1-1)
+         move_right()
+    if c1 > 1:
+        move_left(c1-1)
     
     c2 = 1
     while not wall_is_beneath():
          c2 += 1
-         move_down() 
-    move_up(c2-1)
+         move_down()
+    if c2 > 1:
+        move_up(c2-1)
     
     for i in range(c2):
         for j in range(c1-1):
@@ -25,7 +27,8 @@ def task_5_10():
         fill_cell()
         if not wall_is_beneath():
             move_down()
-        move_left(n=c1-1)    
+        if c1 > 1:
+            move_left(c1-1)
  
 
 if __name__ == '__main__':
