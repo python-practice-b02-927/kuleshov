31d0f971da4c9cebd85cc4f76cb429387cb19d84
StasKuleshov <kuleshov.se@phystech.edu>
2019-09-16 19:52:16 +0300

    task 31

--- lab1/task_31.py
+++ lab1/task_31.py
@@ -5,22 +5,23 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    
-    a = 1
-    while a == 1:
-		
+    a = True
+
+    while a:
         while not wall_is_on_the_right():
             move_right()
-            
+
         while not wall_is_on_the_left():
             if not wall_is_beneath():
                 move_down()
                 break
             move_left()
-        
-        if wall_is_on_the_left() and wall_is_beneath():
-            a = 0
-            
+
+        if wall_is_on_the_left():
+            if wall_is_beneath():
+                a = False
+            else:
+                move_down()
 
 
 if __name__ == '__main__':
