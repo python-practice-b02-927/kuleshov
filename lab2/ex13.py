83bb81d20ceea2e73152ed8dc797c5060a3a5078
StasKuleshov <kuleshov.se@phystech.edu>
2019-09-16 20:23:40 +0300

    task 13

--- lab2/ex13.py
+++ lab2/ex13.py
@@ -12,45 +12,69 @@ a = 100
 n = 4
 c = 1
 
+
+def face():
+    pendown()
+    begin_fill()
+    circle(a)
+    color('yellow')
+    end_fill()
+    color('black')
+    penup()
+
+
+def eye():
+    pendown()
+    begin_fill()
+    circle(a / 5)
+    color('blue')
+    end_fill()
+    color('black')
+    penup()
+
+
+def nose():
+    pendown()
+    color('black')
+    width(10)
+    forward(a * 0.5)
+    penup()
+
+
+def mouth():
+    pendown()
+    color('red')
+    width(10)
+    circle(a * 0.5, 180)
+    color('black')
+    penup()
+
+
 shape('turtle')
-begin_fill()
-circle(a)
-color('yellow')
-end_fill()
-penup()
-color('black')
+
+face()
+
 left(90)
 forward(a*1.6)
 left(90)
 forward(a*0.3)
-pendown()
-begin_fill()
-circle(a/5)
-color('blue')
-end_fill()
-color('black')
-penup()
+
+eye()
+
 backward(a*0.6)
-pendown()
-begin_fill()
-circle(a/5)
-color('blue')
-end_fill()
-color('black')
-penup()
+
+eye()
+
 forward(a*0.3)
 left(90)
 forward(a*0.4)
-pendown()
-width(10)
-forward(a*0.5)
-penup()
+
+nose()
+
 forward(a*0.1)
 right(90)
 forward(a*0.5)
 left(90)
-pendown()
-color('red')
-width(10)
-circle(a*0.5,180)
+
+mouth()
 
