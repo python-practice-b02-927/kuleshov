e4a0021e7bdf003c8173be0f3857049cdefe037e
StasKuleshov <kuleshov.se@phystech.edu>
2019-09-16 19:00:31 +0300

    task 19

--- lab1/task_19.py
+++ lab1/task_19.py
@@ -3,25 +3,25 @@
 from pyrob.api import *
 
 
+def move_up_left():
+    while not wall_is_above():
+        move_up()
+    while not wall_is_on_the_left():
+        move_left()
+
+
 @task
 def task_8_29():
     
     while not wall_is_on_the_left():
         move_left()
-    if wall_is_above():
+    if not wall_is_above():
+        move_up_left()
+    else:
         while not wall_is_on_the_right():
             move_right()
         if not wall_is_above():
-            while not wall_is_above():
-                move_up()
-            while not wall_is_on_the_left():
-                move_left()
-    else:
-        while not wall_is_above():
-            move_up()
-        while not wall_is_on_the_left():
-            move_left()
-		
+            move_up_left()
 
 
 if __name__ == '__main__':
