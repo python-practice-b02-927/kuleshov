ee44927b64f54e29f03434e4feab9b9d874ee47c
StasKuleshov <kuleshov.se@phystech.edu>
2019-09-16 19:08:21 +0300

    task 23

--- lab1/task_23.py
+++ lab1/task_23.py
@@ -3,28 +3,27 @@
 from pyrob.api import *
 
 
-@task(delay=0.01)
-def task_6_6():
-
-    move_right()
-    while not wall_is_on_the_right():
-        if not wall_is_above():
-            c = 0
-            while not wall_is_above():
-                move_up()
-                fill_cell()
-                c += 1
-            move_down(n=c)
-        move_right()
+def up_and_back():
     if not wall_is_above():
         c = 0
         while not wall_is_above():
             move_up()
             fill_cell()
             c += 1
-        move_down(n=c)       
+        move_down(n=c)
+
+
+@task(delay=0.01)
+def task_6_6():
+
+    move_right()
+    while not wall_is_on_the_right():
+        up_and_back()
+        move_right()
+    up_and_back()
     while wall_is_beneath():
         move_left()
+
 
 if __name__ == '__main__':
     run_tasks()
