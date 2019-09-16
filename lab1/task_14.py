cf63dfb8887613213ab632da7303075b99495e8c
StasKuleshov <kuleshov.se@phystech.edu>
2019-09-16 18:42:30 +0300

    task 14

--- lab1/task_14.py
+++ lab1/task_14.py
@@ -3,21 +3,7 @@
 from pyrob.api import *
 
 
-@task
-def task_8_11():
-    
-    while not wall_is_on_the_right():
-        if not wall_is_above():
-            move_up()
-            fill_cell()
-            move_down()
-        if not wall_is_beneath():
-            move_down()
-            fill_cell()
-            move_up()
-        if wall_is_above() and wall_is_beneath():
-            fill_cell()
-        move_right()
+def move_fill():
     if not wall_is_above():
         move_up()
         fill_cell()
@@ -27,7 +13,15 @@ def task_8_11():
         fill_cell()
         move_up()
     if wall_is_above() and wall_is_beneath():
-        fill_cell() 
+        fill_cell()
+
+@task
+def task_8_11():
+    
+    while not wall_is_on_the_right():
+        move_fill()
+        move_right()
+    move_fill()
 
 
 if __name__ == '__main__':
